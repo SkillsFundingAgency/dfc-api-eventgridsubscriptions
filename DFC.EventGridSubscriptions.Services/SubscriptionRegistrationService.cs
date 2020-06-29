f59c60bd77a03dd3f3be99f3f0d3dd35cfd6bcab
Tom Tollafield <thomas.tollafield@gmail.com>
2020-06-29 13:26:15 +0100

    Fixed code smells

--- DFC.EventGridSubscriptions.Services/SubscriptionRegistrationService.cs
+++ DFC.EventGridSubscriptions.Services/SubscriptionRegistrationService.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
