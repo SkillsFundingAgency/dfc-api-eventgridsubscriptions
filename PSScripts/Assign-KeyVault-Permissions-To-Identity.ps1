294b583642d0d15feabb1bec3d0bfff24662bcbe
olusola-adio <olusola_adio@hotmail.com>
2020-07-01 20:36:07 +0100

    key vault permissions

---
+++ PSScripts/Assign-KeyVault-Permissions-To-Identity.ps1
@@ -0,0 +1,52 @@
+<#
+.SYNOPSIS
+Grants list,get permissions on supplied keyvault to supplied application identity
+
+.DESCRIPTION
+Grants list,get permissions on supplied keyvault to supplied application identity
+
+.PARAMETER KeyVaultName
+Name of the keyvault
+
+.PARAMETER ResourceGroupName
+Name of the resource group
+
+.PARAMETER ServicePrincipalName
+Name of the service principal
+
+.EXAMPLE
+Assign-KeyVault-Permissions-To-Identity.ps1 -KeyVaultName KeyVaultName -ResourceGroupName ResourceGroupName -ServicePrincipalName ServicePrincipalName
+Assign-KeyVault-Permissions-To-Identity.ps1 -ResourceGroupName dfc-dev-shared-rg -KeyVaultName dfc-dev-shared-kv -ServicePrincipalName dfc-dev-api-eventgridsubscriptions-fa -Verbose
+#>
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$KeyVaultName,
+    [Parameter(Mandatory=$true)]
+    [string]$ResourceGroupName,
+    [Parameter(Mandatory=$true)]
+    [string]$ServicePrincipalName
+)
+
+$ServicePrincipalObject = Get-AzADServicePrincipal -DisplayName $ServicePrincipalName
+
+if ($ServicePrincipalObject) {
+    Write-Verbose "Setting Application Id for $($ServicePrincipalName)"
+    $ApplicationId = $ServicePrincipalObject.ApplicationId
+
+    Write-Verbose "Removing any existing Azure KeyVault Policy for System Identity $($ServicePrincipalName)"
+    Remove-AzKeyVaultAccessPolicy -VaultName $KeyVaultName -ResourceGroupName $ResourceGroupName -ServicePrincipalName $ApplicationId
+
+    Write-Verbose "Setting Azure KeyVault Policy for System Identity $($ServicePrincipalName) to list,get"    
+    Set-AzKeyVaultAccessPolicy -VaultName $KeyVaultName -ResourceGroupName $ResourceGroupName -ServicePrincipalName $ApplicationId -PermissionsToSecrets  get,list
+
+} else {
+    Write-Verbose "$($ServicePrincipalName) not found on subscription"
+}
+
+
+
+
+
+
+  
