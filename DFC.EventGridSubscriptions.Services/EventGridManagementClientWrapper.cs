6a5a8a8d5f0daf35c93d289b1e37d6c5864c9ebc
Tom Tollafield <thomas.tollafield@gmail.com>
2020-07-01 13:27:12 +0100

    - remove commented code

--- DFC.EventGridSubscriptions.Services/EventGridManagementClientWrapper.cs
+++ DFC.EventGridSubscriptions.Services/EventGridManagementClientWrapper.cs
@@ -35,7 +35,6 @@ namespace DFC.EventGridSubscriptions.Services
 
         public async Task<IEnumerable<EventSubscription>> Subscription_GetAllAsync(string resourceGroupName, string topicName, CancellationToken cancellationToken = default)
         {
-            //var result = await client.Topics.GetAsync(resourceGroupName, topicName, cancellationToken);
             //SDK appears out of line with web reference:
             //https://docs.microsoft.com/en-us/rest/api/eventgrid/version2020-06-01/eventsubscriptions/listbyresource
             var result = await client.EventSubscriptions.ListByResourceAsync(resourceGroupName, string.Empty, "Microsoft.EventGrid", "/topics/" + topicName, null, null, cancellationToken);
