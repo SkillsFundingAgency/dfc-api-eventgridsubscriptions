8071311ded127ac1898764175356e1b7472cc209
Tom Tollafield <thomas.tollafield@gmail.com>
2020-07-02 10:29:17 +0100

    - Change to topic name - Add Environment Variable to Launch Settings

--- DFC.EventGridSubscriptions.Data/EventGridSubscriptionClientOptions.cs
+++ DFC.EventGridSubscriptions.Data/EventGridSubscriptionClientOptions.cs
@@ -5,11 +5,8 @@ namespace DFC.EventGridSubscriptions.Data
     [ExcludeFromCodeCoverage]
     public class EventGridSubscriptionClientOptions
     {
-        public string? ApplicationId { get; set; }
-        public string? ClientSecret { get; set; }
-        public string? TenantId { get; set; }
         public string? ResourceGroup { get; set; }
-        public string? Topic { get; set; }
+        public string? TopicName { get; set; }
         public string? SubscriptionId { get; set; }
     }
 }
