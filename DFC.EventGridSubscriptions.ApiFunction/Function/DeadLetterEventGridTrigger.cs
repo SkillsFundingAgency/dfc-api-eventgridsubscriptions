1ad927c717d6300f60959252c62cfa980cc46d2e
Tom Tollafield <thomas.tollafield@gmail.com>
2020-08-04 13:35:17 +0100

    - Change namespace

--- DFC.EventGridSubscriptions.ApiFunction/Function/DeadLetterEventGridTrigger.cs
+++ DFC.EventGridSubscriptions.ApiFunction/Function/DeadLetterEventGridTrigger.cs
@@ -6,7 +6,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 
-namespace DFC.EventGridSubscriptions.ApiFunction.Function
+namespace DFC.EventGridSubscriptions.ApiFunction
 {
     public static class DeadLetterEventGridTrigger
     {
