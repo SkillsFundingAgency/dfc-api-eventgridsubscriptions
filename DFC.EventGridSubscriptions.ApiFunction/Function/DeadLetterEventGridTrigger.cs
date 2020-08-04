5a1a2e34f9ef31295b00b83276630ceb841a545b
Tom Tollafield <thomas.tollafield@gmail.com>
2020-08-04 15:20:32 +0100

    - Add Blob processor to get dead letter information

--- DFC.EventGridSubscriptions.ApiFunction/Function/DeadLetterEventGridTrigger.cs
+++ DFC.EventGridSubscriptions.ApiFunction/Function/DeadLetterEventGridTrigger.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.Azure.EventGrid.Models;
+﻿using DFC.EventGridSubscriptions.Data;
+using Microsoft.Azure.EventGrid.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.IO;
+using System.Net;
 
 namespace DFC.EventGridSubscriptions.ApiFunction
 {
@@ -37,8 +40,41 @@ namespace DFC.EventGridSubscriptions.ApiFunction
 
                     log.LogInformation("Reading blob data from storage account..");
                     log.LogInformation($"Blob Data:{JsonConvert.SerializeObject(egEvent)}");
+
+                    ProcessBlob(data!.Url, log);
                 }
             }
+        }
+
+        /*
+        This function uses the blob url/location received in the BlobCreated event to fetch data from the storage container.
+        Here, we perform a simple GET request on the blob url and deserialize the dead lettered events in a json array.
+        */
+        public static void ProcessBlob(string url, ILogger log)
+        {
+            // sas key generated through the portal for your storage account used for authentication
+            const string sasKey = "--replace-with-the-storage-account-sas-key";
+            string uri = url + sasKey;
+
+            WebClient client = new WebClient();
+
+            Stream data = client.OpenRead(uri);
+            StreamReader reader = new StreamReader(data);
+            string blob = reader.ReadToEnd();
+            data.Close();
+            reader.Close();
+
+            log.LogInformation($"Dead Letter Events:{blob}");
+
+            // deserialize the blob into dead letter events
+            DeadLetterEvent[] dlEvents = JsonConvert.DeserializeObject<DeadLetterEvent[]>(blob);
+
+            foreach (DeadLetterEvent dlEvent in dlEvents)
+            {
+                log.LogInformation($"Printing Dead Letter Event attributes for EventId: {dlEvent.Id}, Dead Letter Reason:{dlEvent.DeadLetterReason}, DeliveryAttempts:{dlEvent.DeliveryAttempts}, LastDeliveryOutcome:{dlEvent.LastDeliveryOutcome}, LastHttpStatusCode:{dlEvent.LastHttpStatusCode}");
+            }
+
+            client.Dispose();
         }
     }
 }
