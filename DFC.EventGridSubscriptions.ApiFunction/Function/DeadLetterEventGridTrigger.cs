20cd2ce96a7afed3ff5fe38098efc4b951bbf817
Tom Tollafield <thomas.tollafield@gmail.com>
2020-08-05 10:01:58 +0100

    - Change to use blob trigger

--- DFC.EventGridSubscriptions.ApiFunction/Function/DeadLetterEventGridTrigger.cs
+++ DFC.EventGridSubscriptions.ApiFunction/Function/DeadLetterEventGridTrigger.cs
@@ -1,84 +1,86 @@
-﻿using DFC.EventGridSubscriptions.Data;
-using Microsoft.Azure.EventGrid.Models;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+﻿using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
 using System.IO;
-using System.Net;
 
 namespace DFC.EventGridSubscriptions.ApiFunction
 {
     public static class DeadLetterEventGridTrigger
     {
         [FunctionName("ProcessDeadLetter")]
-        public static void Run([EventGridTrigger]JObject eventGridEvent, ILogger log)
+        public static void Run([BlobTrigger("event-grid-dead-letter-events/dfc-dev-stax-egt")] Stream myBlob, string name, ILogger log)
         {
-            if (eventGridEvent == null)
+            if (myBlob == null)
             {
-                throw new ArgumentNullException(nameof(eventGridEvent));
+                throw new ArgumentNullException(nameof(myBlob));
             }
 
-            log.LogInformation($"Event grid trigger function has begun...");
-            const string StorageBlobCreatedEvent = "Microsoft.Storage.BlobCreated";
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
 
-            log.LogInformation(eventGridEvent.ToString(Formatting.Indented));
-            var egEvent = eventGridEvent.ToObject<EventGridEvent>();
+            //if (eventGridEvent == null)
+            //{
+            //    throw new ArgumentNullException(nameof(eventGridEvent));
+            //}
 
-            log.LogInformation($"Event is:{egEvent}");
-            if (egEvent != null && egEvent.Data != null)
-            {
-                log.LogInformation("Event and Data Not Null...");
-                JObject? dataObject = egEvent.Data as JObject;
+            //log.LogInformation($"Event grid trigger function has begun...");
+            //const string StorageBlobCreatedEvent = "Microsoft.Storage.BlobCreated";
 
-                log.LogInformation($"Event Type: {egEvent.EventType}");
+            //log.LogInformation(eventGridEvent.ToString(Formatting.Indented));
+            //var egEvent = eventGridEvent.ToObject<EventGridEvent>();
 
-                if (string.Equals(egEvent.EventType, StorageBlobCreatedEvent, StringComparison.OrdinalIgnoreCase))
-                {
-                    log.LogInformation("Received blob created event..");
+            //log.LogInformation($"Event is:{egEvent}");
+            //if (egEvent != null && egEvent.Data != null)
+            //{
+            //    log.LogInformation("Event and Data Not Null...");
+            //    JObject? dataObject = egEvent.Data as JObject;
 
-                    var data = dataObject?.ToObject<StorageBlobCreatedEventData>();
-                    log.LogInformation($"Dead Letter Blob Url:{data?.Url}");
+            //    log.LogInformation($"Event Type: {egEvent.EventType}");
 
-                    log.LogInformation("Reading blob data from storage account..");
-                    log.LogInformation($"Blob Data:{JsonConvert.SerializeObject(egEvent)}");
+            //    if (string.Equals(egEvent.EventType, StorageBlobCreatedEvent, StringComparison.OrdinalIgnoreCase))
+            //    {
+            //        log.LogInformation("Received blob created event..");
 
-                    ProcessBlob(data!.Url, log);
-                }
-            }
+            //        var data = dataObject?.ToObject<StorageBlobCreatedEventData>();
+            //        log.LogInformation($"Dead Letter Blob Url:{data?.Url}");
+
+            //        log.LogInformation("Reading blob data from storage account..");
+            //        log.LogInformation($"Blob Data:{JsonConvert.SerializeObject(egEvent)}");
+
+            //        ProcessBlob(data!.Url, log);
+            //    }
+            //}
         }
 
         /*
         This function uses the blob url/location received in the BlobCreated event to fetch data from the storage container.
         Here, we perform a simple GET request on the blob url and deserialize the dead lettered events in a json array.
         */
-        public static void ProcessBlob(string url, ILogger log)
-        {
-            // sas key generated through the portal for your storage account used for authentication
-            const string sasKey = "--replace-with-the-storage-account-sas-key";
-            string uri = url + sasKey;
 
-            WebClient client = new WebClient();
+        //public static void ProcessBlob(string url, ILogger log)
+        //{
+        //    // sas key generated through the portal for your storage account used for authentication
+        //    const string sasKey = "--replace-with-the-storage-account-sas-key";
+        //    string uri = url + sasKey;
 
-            Stream data = client.OpenRead(uri);
-            StreamReader reader = new StreamReader(data);
-            string blob = reader.ReadToEnd();
-            data.Close();
-            reader.Close();
+        //    WebClient client = new WebClient();
 
-            log.LogInformation($"Dead Letter Events:{blob}");
+        //    Stream data = client.OpenRead(uri);
+        //    StreamReader reader = new StreamReader(data);
+        //    string blob = reader.ReadToEnd();
+        //    data.Close();
+        //    reader.Close();
 
-            // deserialize the blob into dead letter events
-            DeadLetterEvent[] dlEvents = JsonConvert.DeserializeObject<DeadLetterEvent[]>(blob);
+        //    log.LogInformation($"Dead Letter Events:{blob}");
 
-            foreach (DeadLetterEvent dlEvent in dlEvents)
-            {
-                log.LogInformation($"Printing Dead Letter Event attributes for EventId: {dlEvent.Id}, Dead Letter Reason:{dlEvent.DeadLetterReason}, DeliveryAttempts:{dlEvent.DeliveryAttempts}, LastDeliveryOutcome:{dlEvent.LastDeliveryOutcome}, LastHttpStatusCode:{dlEvent.LastHttpStatusCode}");
-            }
+        //    // deserialize the blob into dead letter events
+        //    DeadLetterEvent[] dlEvents = JsonConvert.DeserializeObject<DeadLetterEvent[]>(blob);
 
-            client.Dispose();
-        }
+        //    foreach (DeadLetterEvent dlEvent in dlEvents)
+        //    {
+        //        log.LogInformation($"Printing Dead Letter Event attributes for EventId: {dlEvent.Id}, Dead Letter Reason:{dlEvent.DeadLetterReason}, DeliveryAttempts:{dlEvent.DeliveryAttempts}, LastDeliveryOutcome:{dlEvent.LastDeliveryOutcome}, LastHttpStatusCode:{dlEvent.LastHttpStatusCode}");
+        //    }
+
+        //    client.Dispose();
+        //}
     }
 }
